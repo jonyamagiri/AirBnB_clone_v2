6c8e35a4f0cf372f2daca8674d02df897114a67e
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-02-21 20:16:20 +0000

    web_flask

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application; listens on 0.0.0.0, port 5000
+uses storage for fetching data from the storage engine
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """Render state_list html page to display States created"""
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Method to remove current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
